0e59730a4f6e4ddc2827f63d64147661d0314b88
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 12:15:50 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -108,14 +108,11 @@ module Players
 
       Game::WIN_COMBINATIONS.detect do |combination|
 
-        if board.cells[combination[0]] == player_token &&
-           board.cells[combination[0]] == board.cells[combination[1]] &&
+        if board.cells[combination[0]] == player_token && board.cells[combination[0]] == board.cells[combination[1]] &&
           combination[2] + 1
-        elsif board.cells[combination[0]] == player_token &&
-              board.cells[combination[0]] == board.cells[combination[2]]
+        elsif board.cells[combination[0]] == player_token && board.cells[combination[0]] == board.cells[combination[2]]
           combination[1] + 1
-        elsif board.cells[combination[1]] == player_token &&
-              board.cells[combination[0]] == board.cells[combination[2]]
+        elsif board.cells[combination[1]] == player_token && board.cells[combination[0]] == board.cells[combination[2]]
           [combination[0] + 1
 
         end # if combination.index
@@ -124,7 +121,7 @@ module Players
 
     def place_my_token(board)
       potential_combos(board, self.token)
-    end #
+    end # 
 
 
     def check_corners(board)
