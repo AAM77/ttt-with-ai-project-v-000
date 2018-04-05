d2e8ae419140919376ebab3591e6d7f3c916c788
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 12:53:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -88,8 +88,9 @@ module Players
 
 
     def go_for_victory(board)
-      index ||= potential_combos(board, self.token)
-      position ||= (index + 1).to_s
+      index = potential_combos(board, self.token)
+      if !indec.nil?
+      position = (index + 1).to_s
       if board.valid_move?(position)
         position
       end # if valid_move?
