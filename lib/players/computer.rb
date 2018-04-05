5e53b3ca2c79d3000e487ab5c9cd6ac94bf96244
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 14:21:52 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -78,6 +78,8 @@ module Players
         if board.cells[combination[0]] == token_to_test && board.cells[combination[1]] == token_to_test
           if !board.taken?(combination[2] + 1)
             (combination[2] + 1)
+          else
+            nil
           end # if taken?
         elsif board.cells[combination[0]] == token_to_test && board.cells[combination[2]] == token_to_test
           if !board.taken?(combination[1] + 1)
