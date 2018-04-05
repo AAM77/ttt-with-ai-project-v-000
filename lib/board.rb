6591ae683f9bc7d1aa4c8d145b05cc0998fce097
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 16:19:17 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -7,6 +7,7 @@ class Board
     self.cells ||= Array.new(9, " ")
   end # initialize
 
+
   ## displays the board
   def display
 
@@ -18,20 +19,24 @@ class Board
 
   end # board
 
+
   ## resets the array if necessary
   def reset!
     self.cells = Array.new(9, " ")
   end # reset!
+
 
   ## determines the value in the input position
   def position(user_input)
     cells[user_input.to_i - 1]
   end # position
 
+
   ## determines if the board is full
   def full?
     true if cells.all?{|pos| pos == "X" || pos == "O"}
   end # full?
+
 
   ## determines what turn it is now
   def turn_count
@@ -40,16 +45,19 @@ class Board
     count
   end # turn_count
 
+
   ## determines if the spot has been taken
   def taken?(user_input)
     position(user_input) == "X" || position(user_input) == "O" ? true : false
   end # taken?
+
 
   ## determines if the entered input is of the specified parameters
   def valid_move?(user_input)
     index = user_input.to_i - 1
     true if (0 <= index && index <= 8) && taken?(user_input) == false
   end # valid_move?
+
 
   ## updates the board by placing the user's token in the desired position
   def update(user_input, player)
