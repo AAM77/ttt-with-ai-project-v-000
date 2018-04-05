2768484f96aa4d16e7a55b08cadc3f03978c1892
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 13:50:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -75,7 +75,8 @@ module Players
 
       Game::WIN_COMBINATIONS.each do |combination|
         if board.cells[combination[0]] == token_to_test && board.cells[combination[1]] == token_to_test
-          if !board.taken?(combination[2])
+          if !board.taken?(combination[2] + 1)
+            combination[2] + 1
             
 
       winning_combo = []
