a5bf48b95194867b7ba4abb10ad2d1641da25a79
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 12:40:05 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -76,9 +76,9 @@ module Players
       Game::WIN_COMBINATIONS.detect do |combination|
 
         if board.cells[combination[0]] == player_token && board.cells[combination[0]] == board.cells[combination[1]]
-          combination[2] + 1
+          if !board.taken?((combination[2] + 1).to_s)
         elsif board.cells[combination[0]] == player_token && board.cells[combination[0]] == board.cells[combination[2]]
-          combination[1] + 1
+          if !board.taken? combination[1] + 1.to_s
         elsif board.cells[combination[1]] == player_token && board.cells[combination[1]] == board.cells[combination[2]]
           combination[0] + 1
         end # if combination index == token
