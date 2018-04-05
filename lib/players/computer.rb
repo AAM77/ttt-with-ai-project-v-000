d0d6332fb68c6c0ceb1c7d9b5bd0b1f7e707b358
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 12:22:14 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -105,36 +105,9 @@ module Players
 
 
     def check_middle(board)
-      value = rand(1..8)
       if !board.taken?("5")
         "5"
-      elsif board.valid_move?(value)
-        value.to_s
-      elsif board.valid_move?("1")
-        "1"
-      elsif board.valid_move?("3")
-        "3"
-      elsif board.valid_move?("7")
-        "7"
-      elsif board.valid_move?("9")
-        "9"
-      elsif board.valid_move?("1")
-        "1"
-      elsif board.valid_move?("3")
-        "3"
-      elsif board.valid_move?("7")
-        "7"
-      elsif board.valid_move?("9")
-        "9"
-      elsif board.valid_move?("6")
-        "6"
-      elsif board.valid_move?("4")
-        "4"
-      elsif board.valid_move?("8")
-        "8"
-      elsif board.valid_move?("2")
-        "2"
-      end # if
+      end # if middle_taken
     end # check_middle
 
 
