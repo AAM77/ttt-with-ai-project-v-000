4da0187725387ab5711a07aa48941a845a4b0504
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-05 15:58:15 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@
 ## Need to reduce repeition
 ## Need to add in a method that places at random
 ## Need to make it so that the computer results in cat's game each time.
-#### ---> Currently, the only way to do this has been to run only the check middle and place_elsewhere methods
+#### ---> Currently, the only way to do this has been to run only the 'check_middle' and 'place_elsewhere' methods
 ## I will come back to this later on and get it to work the way it should
 
 
